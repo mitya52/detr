7d683c7ea5e6bec6f5a4c764802ed3c0cbf8796d
mitya <dimitry.ageev@gmail.com>
2020-06-07 04:21:17 +0300

    keypoints hungarian matcher added

--- models/matcher.py
+++ models/matcher.py
@@ -82,5 +82,83 @@ class HungarianMatcher(nn.Module):
         return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
 
 
+class KeypointsHungarianMatcher(nn.Module):
+    """This class computes an assignment between the targets and the predictions of the network
+
+    For efficiency reasons, the targets don't include the no_object. Because of this, in general,
+    there are more predictions than targets. In this case, we do a 1-to-1 matching of the best predictions,
+    while the others are un-matched (and thus treated as non-objects).
+    """
+
+    def __init__(self, cost_class: float = 1, cost_confidence: float = 1, cost_location: float = 1):
+        """Creates the matcher
+
+        Params:
+            cost_class: This is the relative weight of the classification error in the matching cost
+            cost_location: This is the relative weight of the location loss of the pose in the matching cost
+            cost_confidence: This is the relative weight of the confidence loss of the pose in the matching cost
+        """
+        super().__init__()
+        self.cost_class = cost_class
+        self.cost_confidence = cost_confidence
+        self.cost_location = cost_location
+        assert cost_class != 0 or cost_confidence != 0 or cost_location != 0 , "all costs cant be 0"
+
+    @torch.no_grad()
+    def forward(self, outputs, targets):
+        """ Performs the matching
+
+        Params:
+            outputs: This is a dict that contains at least these entries:
+                 "pred_logits": Tensor of dim [batch_size, num_queries, num_classes] with the classification logits
+                 "pred_keypoints": Tensor of dim [batch_size, num_queries, numn_keypoints * 3] with the predicted pose
+
+            targets: This is a list of targets (len(targets) = batch_size), where each target is a dict containing:
+                 "labels": Tensor of dim [num_target_boxes] (where num_target_boxes is the number of ground-truth
+                           objects in the target) containing the class labels
+                 "keypoints": Tensor of dim [num_target_boxes, num_keypoints * 3] containing the target pose
+
+        Returns:
+            A list of size batch_size, containing tuples of (index_i, index_j) where:
+                - index_i is the indices of the selected predictions (in order)
+                - index_j is the indices of the corresponding selected targets (in order)
+            For each batch element, it holds:
+                len(index_i) = len(index_j) = min(num_queries, num_target_boxes)
+        """
+        bs, num_queries = outputs["pred_logits"].shape[:2]
+
+        # We flatten to compute the cost matrices in a batch
+        out_prob = outputs["pred_logits"].flatten(0, 1).softmax(-1)  # [batch_size * num_queries, num_classes]
+        out_conf = outputs["pred_confidence"].flatten(0, 1)  # [batch_size * num_queries, num_keypoints]
+        out_loc = outputs["pred_location"].flatten(0, 1)  # [batch_size * num_queries, num_keypoints * 2]
+        out_loc = out_loc.view(out_loc.size(0), -1, 2)
+
+        # Also concat the target labels and boxes
+        tgt_ids = torch.cat([v["labels"] for v in targets])
+        tgt_conf = torch.cat([v["keypoints"][..., 2] for v in targets])
+        tgt_loc = torch.cat([v["keypoints"][..., :2] for v in targets])
+
+        # Compute the classification cost. Contrary to the loss, we don't use the NLL,
+        # but approximate it in 1 - proba[target class].
+        # The 1 is a constant that doesn't change the matching, it can be ommitted.
+        cost_class = -out_prob[:, tgt_ids]
+
+        # Compute the L1 cost between confidence
+        cost_confidence = torch.cdist(out_conf, tgt_conf, p=1)
+
+        # Compute the L1 cost between locations
+        cost_location = cost_confidence.new_zeros((out_loc.size(0), tgt_loc.size(0)))
+        for idx, conf, loc in enumerate(zip(tgt_conf, tgt_loc)):
+            cost_location[:, idx] = torch.cdist(out_loc[conf > 0], loc[conf > 0], p=1)
+
+        # Final cost matrix
+        C = self.cost_class * cost_class + self.cost_confidence * cost_confidence + self.cost_location * cost_location
+        C = C.view(bs, num_queries, -1).cpu()
+
+        sizes = [len(v["keypoints"]) for v in targets]
+        indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
+        return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
+
+
 def build_matcher(args):
     return HungarianMatcher(cost_class=args.set_cost_class, cost_bbox=args.set_cost_bbox, cost_giou=args.set_cost_giou)
